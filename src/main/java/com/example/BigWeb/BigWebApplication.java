55c6ea4b8860f0e9d9f082cf82c6719f3290549a
v.zineev@tsbnk.ru <vadimz026@gmail.com>
2021-05-04 11:03:52 +0300

    load project

---
+++ src/main/java/com/example/BigWeb/BigWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.BigWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigWebApplication.class, args);
+	}
+
+}
